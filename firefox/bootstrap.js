0628ec8718a6332546533d39e255092e3c7e5614
Greg Leuch <contact@gleuch.com>
2012-08-15 10:35:10 +0800

    ...and the Olwimpics have come to a close. Thank you all! See you in Rio in four years! (v 1.0.6 -- removing all code)

--- firefox/bootstrap.js
+++ firefox/bootstrap.js
@@ -20,6 +20,8 @@
   #######################################################################################
 
 
+            !!! FINAL VERSION ### OLYMPICS ARE OVER, SO IS THIS BLOCKER !!!
+
 */
 
 
@@ -35,36 +37,14 @@ Cu.import("resource://gre/modules/NetUtil.jsm");
 /* --- STARTUP ---------------------------------------------------------------- */
 
 function startup({id}) AddonManager.getAddonByID(id, function(addon) {
-  /*
-    Load up watcher window:
-      * Although "scriptloader" gives warning in validation, is noted as being not dangerous:
-        - Noted this as flag for reviewer, not as being harmful, by eviljeff: https://forums.mozilla.org/addons/viewtopic.php?p=13146
-        - Listed as not being dangerous anymore, by Jorge Villalobos: https://groups.google.com/d/msg/mozilla.dev.extensions/TMFWetkVEDQ/fbrHUI3CrWgJ
-  */
-  Services.scriptloader.loadSubScript(addon.getResourceURI('content/js/watch.js').spec, global);
-  
-  watchWindows(function(win) {
-    win.addon = addon;
-    win.gBrowser.tabContainer.addEventListener("TabAttrModified", function(e) {
-      try {
-        var tab = this.gBrowser.getBrowserForTab(e.target);
-        if (tab === undefined || !tab.currentURI.spec.match(/^http/i)) return false;
-
-        if (!tab.contentDocument._olwimpics_loaded) {
-          Services.scriptloader.loadSubScript(this.addon.getResourceURI('content/js/jquery-1.7.2.min.js').spec, tab.contentDocument);
-          Services.scriptloader.loadSubScript(this.addon.getResourceURI('content/js/blocker.js').spec, tab.contentDocument);
-          tab.contentDocument._olwimpics_loaded = true;
-        }
-      } catch(e) {}
-    }.bind(win), false);
-  });
+  /* We don't need to startup anything. Olympics is over! */
 });
 
 
 /* --- SHUTDOWN --------------------------------------------------------------- */
 
 function shutdown(data, reason) {
-  /* We don't nee to shutdown anything. Everything is handled in the startup */
+  /* We don't need to shutdown anything. Everything is handled in the startup */
 };
 
 
